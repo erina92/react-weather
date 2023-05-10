51005b9b9b6eb78e5b55192b0e75c18869a1bff1
erina92 <102061304+erina92@users.noreply.github.com>
2023-05-10 13:49:18 +0200

    finished html

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,26 @@
 import React from "react";
 import PartlyCloudyDay from "./icons/partly-cloudy-day.svg";
+import Humidity from "./icons/humidity.svg";
+import Wind from "./icons/wind.svg";
 import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>Taranto</h1>
       <ul>
         <li>Wednesday 10:23</li>
@@ -12,13 +28,17 @@ export default function Weather() {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img src={PartlyCloudyDay} alt="Cloudy" />
+          <img src={PartlyCloudyDay} alt="Cloudy" width={100} height={100} />
           18°C
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: 15%</li>
-            <li>Wind: 13 km/h</li>
+            <li>
+              <img src={Humidity} alt="humidity" width={50} height={50} />: 15%
+            </li>
+            <li>
+              <img src={Wind} alt="wind" width={50} height={50} />: 13 km/h
+            </li>
           </ul>
         </div>
       </div>
